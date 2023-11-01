820e60ff7c7c4c5b1152be9d69c9ff01d0b79dee
Renaldy002 <oshaleon581@Gmail.com>
2023-11-01 22:46:56 +0700

    masuk pdf

--- src/app/kepailitan-pt-kaiso-maritim-global-dalam-pailit/kepailitan-pt-kaiso-maritim-global-dalam-pailit.component.ts
+++ src/app/kepailitan-pt-kaiso-maritim-global-dalam-pailit/kepailitan-pt-kaiso-maritim-global-dalam-pailit.component.ts
@@ -1,4 +1,5 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import { NgxExtendedPdfViewerService, pdfDefaultOptions } from 'ngx-extended-pdf-viewer';
 
 @Component({
   selector: 'app-kepailitan-pt-kaiso-maritim-global-dalam-pailit',
@@ -7,5 +8,17 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class KepailitanPtKaisoMaritimGlobalDalamPailitComponent {
+  
+  constructor(private pdfService: NgxExtendedPdfViewerService) {
+    /* More likely than not you don't need to tweak the pdfDefaultOptions.
+       They are a collecton of less frequently used options.
+       To illustrate how they're used, here are two example settings: */
+    pdfDefaultOptions.doubleTapZoomFactor = '200%'; // The default value is '200%'
+    pdfDefaultOptions.maxCanvasPixels = 4096 * 4096 * 5; // The default value is 4096 * 4096 pixels,
+    // but most devices support much higher resolutions.
+    // Increasing this setting allows your users to use higher zoom factors,
+    // trading image quality for performance.
 
+    
+    }
 }
